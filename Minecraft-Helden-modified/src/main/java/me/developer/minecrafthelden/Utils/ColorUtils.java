32adc012504cddf9b868683868f71b6e60f48a84
ElocinYT <kuroshjoel.senf@icloud.com>
2026-08-08 17:15:59 +0100

    Delete Minecraft-Helden-modified directory

--- Minecraft-Helden-modified/src/main/java/me/developer/minecrafthelden/Utils/ColorUtils.java
+++
@@ -1,9 +0,0 @@
-package me.developer.minecrafthelden.Utils;
-
-import org.bukkit.ChatColor;
-
-public class ColorUtils {
-    public static String translateColors(String message) {
-        return ChatColor.translateAlternateColorCodes('&', message);
-    }
-}
